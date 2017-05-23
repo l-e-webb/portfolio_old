2ea106cf7bb5282ddf2b0a82839b2707479512fd
Louis <louis.webb5@gmail.com>
2017-05-22 17:55:02 -0700

    Make entries with javascript

---
+++ js/portfolioCreator.js
@@ -0,0 +1,98 @@
+(function() {
+
+    var entryHtml = '<div class="row">\
+            <div class="col-md-4 imagecolumn">\
+                <img src="images/%img%" class="portfolioimg">\
+            </div>\
+            <div class="col-md-8" blurbcolumn>\
+                <h4>%name%</h4>\
+                <p>%description%</p>\
+                <h5>Credits</h5>\
+                <p>%credits%</p>\
+                <p>%links%</p>\
+            </div>\
+        </div>\
+        <hr>'
+    var linkHtml = '<a href="%link%" target="_blank">%link text%</a>'
+
+    function displayEntry(entryData) {
+        var entry = entryHtml.replace("%name%", entryData.name)
+                .replace("%img%", entryData.img)
+                .replace("%description%", entryData.description)
+                .replace("%credits%", entryData.credits);
+        
+        var links = "";
+        for (var i = 0; i < entryData.links.length; i++) {
+            if (i > 0) links += "<br>";
+            var linkData = entryData.links[i];
+            links += linkHtml.replace("%link text%", linkData[0])
+                    .replace("%link%", linkData[1]);
+        }
+        entry = entry.replace("%links%", links);
+
+        $("#footer1").before(entry);
+    }
+
+    var portfolioEntries = [];
+    portfolioEntries.push(
+    {
+        "name": "The Blue Room",
+        "img": "the_blue_rooM_project_pic.png",
+        "description": "A re-imagining of the classic room-escape adventure game genre, The Blue Room paints a world where the true impediment to escape is the protagonist's own depression and agoraphobia. Explore the vast yet sharply limiting confines of a single room, and join the protagonist as they search for hope, motivation, joy, or just distraction from their depression.  The art features digital collage techniques.",
+        "credits": 'Concept, design, writing, programming, art: Louis Webb<br>Made with Unity using Tony Coculuzzi\'s <a href="https://www.assetstore.unity3d.com/en/#!/content/14854" target="_blank">Dialoguer</a>',
+        "links": [
+            ["Play", "https://l-e-webb.github.io/the_blue_room"],
+            ["Download", "https://www.dropbox.com/sh/e8z11m8qbnhry5a/AAC6ecBL7YsdkElWABzDezBya"]
+        ]
+    });
+
+    portfolioEntries.push(
+    {
+        "name": "Asteroidal Projection",
+        "img": "asteroidal_projection_project_pic.png",
+        "description": "Asteroidal Projection is inspired by the classic arcade game Asteroids, but comes with a twist.  The game occurs in the normal Euclidean plane projected into the unit disc.  An entire infinite 2D space is inside the disc-shaped play area.  Sizes and distances are distorted the farther you get from the center.  Objects will appear to shrink and go slower as they near the edges of the disc.  This game is currently under development, but you can play the demo online.",
+        "credits": "Concept, design, programming: Louis Webb<br>Made with libGDX",
+        "links": [
+            ["Play demo", "https://l-e-webb.github.io/asteroidal_projection"],
+            ["Source", "https://github.com/l-e-webb/asteroidal_projection"]
+        ]
+    });
+    portfolioEntries.push(
+    {
+        "name": "CrossFade",
+        "img": "crossfade_project_pic.png",
+        "description": ">A simple puzzle game.  Selecting a tile flips the on/off value of all the tiles in that tile's row and column.  Try to turn them all off!  The game is playable in-browser and on Android.  (Not on Google Play Store at the moment, but hopefully soon!)",
+        "credits": "Concept, design, programming: Louis Webb<br>Made with libGDX",
+        "links": [
+            ["Play", "https://l-e-webb.github.io/crossfade"],
+            ["Source", "https:github.com/l-e-webb/crossfade"]
+        ]
+    });
+    portfolioEntries.push(
+    {
+        "name": "Space Kerfuffle!",
+        "img": "space_kerfuffle_project_pic.png",
+        "description": 'Space Kerfuffle! is a top-down starfighter game.  Blast your way through hoards of enemy ships and destroy the mothership!  This game was created as a sample implementation for students in the <a href="https://udacity.com" target="_blank">Udacity</a> 2D Mobile Game Developer Nanodegree.',
+        "credits": 'Concept, design: Louis Webb, Udacity 2D Mobile Game Developer Nanodegree content team<br>Art: <a href="https://github.com/ShengKungYi" target="_blank">Mike Yi</a><br>Programming: Louis Webb<br>Made with libGDX',
+        "links": [
+            ["Play", "https://l-e-webb.github.io/space_kerfuffle"],
+            ["Source", "https://github.com/l-e-webb/space_kerfuffle"]
+        ]
+    });
+    portfolioEntries.push(
+    {
+        "name": "Outbreak",
+        "img": "outbreak_project_pic.png",
+        "description": 'Outbreak is a simple cross-platform (browser/desktop/Android) Breakout clone.  It was created as a sample implementation for students in the <a href="https://udacity.com" target="_blank">Udacity</a> 2D Mobile Game Developer Nanodegree.',
+        "credits": "Concept, design: Louis Webb, Udacity 2D Mobile Game Developer Nanodegree content team<br>Programming: Louis Webb<br>Made with libGDX",
+        "links": [
+            ["Play", "https://l-e-webb.github.io/outbreak"],
+            ["Source", "https://github.com/l-e-webb/outbreak"]
+        ]
+    });
+
+    for (var i = 0; i < portfolioEntries.length; i++) {
+        displayEntry(portfolioEntries[i]);
+    }
+
+})();
